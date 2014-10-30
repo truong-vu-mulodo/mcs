e4be717c1388c7ecc2b3b58bdbc7c9d12341aadf
Vu Truong <truong.vu@mulodo.com>
2014-10-30 16:17:30 +0700

    Add controller and model code for "Create User Account"

--- src/fuel/app/classes/controller/v1/user.php
+++ src/fuel/app/classes/controller/v1/user.php
@@ -1,11 +1,15 @@
 <?php
+// namespace Controller\V1;
+
+use Fuel\Core\Controller_Rest;
+use \Model\V1\User;
 /**
  * The User Controller version v1.
  *
  * It controls all requests of API endpoints that configed in routes.php.
  *
  * @package  app
- * @extends  Controller
+ * @extends  Controller_Rest
  */
 class Controller_V1_User extends Controller_Rest {
 
@@ -20,16 +24,18 @@ class Controller_V1_User extends Controller_Rest {
 	 * @return  Response
 	 */
 	public function post_create() {
-		return $this->response(
-			array(
-				'username' => Input::post('username')." v1",
-				'password' => Input::post('password'),
-				'firstname' => Input::post('firstname'),
-				'lastname' => Input::post('lastname'),
-				'email' => Input::post('email'),
-			)
+		
+		$user_id = User::create_acount(
+				array(
+					'username' => Input::post('username'),
+					'password' => Input::post('password'),
+					'firstname' => Input::post('firstname'),
+					'lastname' => Input::post('lastname'),
+					'email' => Input::post('email')		
+				)
 		);
+
+		// Return created user id (record id)
+		return $this->response(array('user_id' => $user_id));
 	}
-
-
 }
---
+++ src/fuel/app/classes/model/v1/user.php
@@ -0,0 +1,62 @@
+<?php
+namespace Model\V1;
+
+use Fuel\Core\DB;
+use Fuel\Core\Model;
+/**
+ * The User Model version v1.
+ *
+ * Contains all business logic for Users API endpoints.
+ *
+ * @package  app
+ * @extends  Model
+ */
+class User extends Model {
+
+	/**
+	 * Create user account.
+	 * This will insert new record into "user" table from provided information.
+	 * 
+	 * @access  public
+	 * @return  Response
+	 */
+	public static function create_acount($user_info) {
+		try {
+			// Prepare an insert statement
+			$query = DB::insert('user');
+			
+			// Set the columns
+			$query->columns(array(
+					'username',
+					'password',
+					'firstname',
+					'lastname',
+					'email',
+					'create_dt',
+					'update_dt'
+			));
+			
+			// Set the values
+			$query->values(array(
+					$user_info['username'],
+					$user_info['password'],
+					$user_info['firstname'],
+					$user_info['lastname'],
+					$user_info['email'],
+					time(),
+					time()
+			));
+			
+			// Execute the query and return a new Database_MySQLi_Result
+			$result = $query->execute();
+// 			var_dump($result);
+
+			// Return new record's id
+			return $result[0];
+		} catch (Exception $e) {
+			// Write log here
+		}
+	}
+
+
+}
