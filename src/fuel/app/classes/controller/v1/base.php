ee46d023e26f6eddd388b100cca02c426fbe06d5
Vu Truong <truong.vu@mulodo.com>
2014-10-31 13:53:01 +0700

    - Re-design controller classes.

--- src/fuel/app/classes/controller/v1/base.php
+++ src/fuel/app/classes/controller/v1/base.php
@@ -1,36 +1,16 @@
 <?php
 // namespace Controller;
 
-use Fuel\Core\Controller_Rest;
-// use \Model\V1\User;
-use \Ultility\Error;
 /**
  * The Base User Controller for version v1.
+ * It extends from {@link Controller_Base} class.
  * All common methods for controllers should be declared here.
  *
  * @package  app
- * @extends  Controller_Rest
+ * @extends  Controller_Base
  * @author Vu Truong
  * @since 2014/10/31
  */
-class Controller_V1_Base extends Controller_Rest {
-
-	/**
-	 * Get system error's error content. 
-	 * This will be converted to JSON in response.
-	 *
-	 * @return array Error content for response
-	 */
-	protected function get_system_error_response() {
-		// Return error info by array format
-		return array(
-				'meta' => array(
-						'code' => _SYSTEM_ERROR_CODE_,
-						'description' => "System error.",
-						'messages' => array('message' => _SYSTEM_ERROR_MSG_)
-				),
-				'data' => null
-		);
-	}
+class Controller_V1_Base extends Controller_Base {
 
 }
--- src/fuel/app/classes/controller/v1/user.php
+++ src/fuel/app/classes/controller/v1/user.php
@@ -84,7 +84,7 @@ class Controller_V1_User extends Controller_V1_Base {
 		try {
 			// Create a new validation instance
 			$val = Validation::forge('user');
-	
+
 			// Set rules for input fields
 			$val->add_field('username', 'Username', 'required|trim|valid_string[alpha,lowercase,numeric]');
 			$val->add_field('password', 'Password', 'required|trim|valid_string[alpha,lowercase,numeric]');
